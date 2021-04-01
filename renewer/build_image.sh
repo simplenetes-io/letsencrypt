919390c016154cc8258082b60c4332f279d05eed
Thomas Backlund <blund@blockie.org>
2021-04-01 11:11:14 +0200

    Remove unused build scripts

--- renewer/build_image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-tag="letsencrypt-renewer:0.0.84"
-
-podman build -t "${tag}" .
--- server/build_image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-tag="letsencrypt-server:0.0.1"
-
-podman build -t "${tag}" .
